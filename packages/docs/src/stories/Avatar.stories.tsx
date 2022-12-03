c5100251970620a74009693efc2c141324579487
erickmarquesc <erick.marquesc@hotmail.com>
2022-12-03 14:20:29 -0300

    feat: :sparkles: created Avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import { Avatar, AvatarProps } from '@ignitecall-ui-designsystem/react';
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/erickmarquesc.png',
+    alt: 'Avatar'
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {};
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  }
+};
